eb408e84287c65433d10cf29e490b7bd3d9f3aea
RaxEmRemy <pwnagepal@gmail.com>
2017-08-03 03:49:06 -0400

    Periodic commit.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,21 @@
 
 # INSTALL DEPENDENCIES
 
-# install i3
 sudo apt-get install i3
+sudo apt-get install compton
+sudo apt-get install firefox
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install python
+sudo apt-get install python3
+sudo apt-get install 
+
+
+
+
+
+
+
 
 # clone i3-gaps
 git clone https://github.com/Airblader/i3.git
