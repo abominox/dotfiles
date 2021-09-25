603a6fe9f482d46246728babdd145142b0f60e11
abominox <pwnagepal@gmail.com>
2021-09-25 16:20:44 -0400

    Remove old python systat from scripts folder

--- scripts/systat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""System Statistics Printout for tmux Statusline"""
-
-from datetime import datetime
-import psutil
-
-print(str(psutil.cpu_percent(interval=0.1)) + "/" \
-    + str(psutil.virtual_memory()._asdict().get("percent")) \
-    + str(datetime.now().strftime(' | %r | %a %b %d, %Y')))
